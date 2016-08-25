c46f3c612bf62d7b89e51ce4cdbd2d0967b46b66
Paola Ortiz de Zevallos <paolaozv@gmail.com>
2016-08-25 17:28:53 -0500

    ejercicio 2

---
+++ app/app.js
@@ -0,0 +1,11 @@
+var num1 = prompt("Ingresa el primer numero");
+    num1 = parseInt(num1);
+var num2 = prompt("Ingresa el segundo numero");
+    num2 = parseInt(num2);
+var num3 = prompt("Ingresa el tercer numero");
+    num3 = parseInt(num3);
+var suma = num1 + num2 + num3;
+var promedio = suma/3;
+
+   document.write("Su promedio es: ");
+   document.write(promedio);
